ddf7a91d847c5c41d57aea554e38152ecdb5388f
lachevo <benjaminleul@gmail.com>
2026-06-20 14:01:03 +0300

    added bookList

--- books/views.py
+++ books/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'booksList.html', {'books': books})
